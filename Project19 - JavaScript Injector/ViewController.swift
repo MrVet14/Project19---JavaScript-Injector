d46a375ccb2d03dbd9f87c06733e32716687e486
Vitali <vitali.vyucheiski@gmail.com>
2022-06-13 11:09:21 +0300

    Initial commit

---
+++ Project19 - JavaScript Injector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project19 - JavaScript Injector
+//
+//  Created by Vitali Vyucheiski on 5/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Project19---JavaScriptInjector
+ A Learning Project 19, coded by Me during Hacking with Swift in 100 Days Course
